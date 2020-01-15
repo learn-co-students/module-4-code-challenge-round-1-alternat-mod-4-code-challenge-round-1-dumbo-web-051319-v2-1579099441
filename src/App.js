67a6b4e96d987d3f47712c74f87e94565b54a165
Michelle Wong <michellekwong2@gmail.com>
2020-01-15 10:30:34 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -5,11 +5,53 @@ import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
 class App extends Component {
+  state={
+    books: [],
+    bookShelf: []
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3005/books')
+    .then(resp => resp.json())
+    .then(books =>{
+      this.setState({
+        books: books
+      })
+    })
+  }
+
+  addToBookShelf=(book)=>{
+    if(!this.state.bookShelf.includes(book))
+      {this.setState({
+      bookShelf: [...this.state.bookShelf, book]
+      })}
+  }
+
+  removeFromBookShelf=(book)=>{
+    let filteredBookShelf = this.state.bookShelf.filter(b => {
+      return b !==book
+    })
+    this.setState({
+      bookShelf: filteredBookShelf
+    })
+  }
+
+  addBook=(myBook)=>{
+    this.setState({
+      books: [...this.state.books, myBook]
+    })
+  }
+
   render() {
+    console.log(this.state.books)
     return (
       <div className="book-container">
-        <BookList />
-        <Bookshelf />
+        <BookList books={this.state.books}
+                  handleClick={this.addToBookShelf}
+                  addBook={this.addBook} />
+        <Bookshelf books={this.state.books}
+                  bookShelf={this.state.bookShelf}
+                  handleClick={this.removeFromBookShelf}/>
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,63 @@
 import React from "react";
 
 class Form extends React.Component {
+  state={
+    title: '',
+    author: '',
+    img: ''
+  }
+
+  onChange=(e)=>{
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+   onSubmit=(e)=>{
+    e.preventDefault()
+    let {title, author, img} = this.state
+    fetch('http://localhost:3005/books', {
+      method: "POST",
+      headers:{ 'Content-Type': 'application/json',
+                'Accept': 'application/json'},
+      body: JSON.stringify({
+        title,author,img
+      })
+    })
+    .then(resp => resp.json())
+    .then(myBook => {
+      this.props.addBook(myBook)
+    })
+    this.setState({
+      title: '',
+      author: '',
+      img: ''
+    })
+  }
+
   render() {
-    return <h1>{/*create form*/}</h1>;
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+        <input type="text" 
+                name="title" 
+                placeholder="Title" 
+                value={this.state.title}
+                onChange={e => this.onChange(e)}/>
+        <input type="text" 
+                placeholder="Author"
+                name="author" 
+                value={this.state.author}
+                onChange={e => this.onChange(e)}/>
+        <input type="text" 
+                placeholder="Image" 
+                name="img" 
+                value={this.state.img}
+                onChange={e => this.onChange(e)}/>
+        <button>Submit</button>
+        </form>
+      </div>
+    )
   }
 }
 
